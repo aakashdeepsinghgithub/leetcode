3793bdf838f918d72d59e209365e37bdbcd91f12
aakashdeepsinghgithub <117113183+aakashdeepsinghgithub@users.noreply.github.com>
2025-06-20 14:36:55 +0530

    File updated at 6/20/2025, 2:36:56 PM

--- 2294-PartitionArraySuchThatMaximumDifferenceIsK/2294-PartitionArraySuchThatMaximumDifferenceIsK.java
+++ 2294-PartitionArraySuchThatMaximumDifferenceIsK/2294-PartitionArraySuchThatMaximumDifferenceIsK.java
@@ -1,17 +1,17 @@
-// Last updated: 6/19/2025, 11:38:10 PM
+// Last updated: 6/20/2025, 2:36:56 PM
 class Solution {
     public int partitionArray(int[] nums, int k) {
         Arrays.sort(nums);
-        int count = 1; // At least one group
-        int start = nums[0];
 
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] - start > k) {
+        int count = 1;
+        int start = nums[0];
+        for(int i=0 ; i<nums.length ; i++){ 
+            
+            if(nums[i] - start > k){ 
                 count++;
-                start = nums[i];
+                start=nums[i];
             }
         }
-
         return count;
     }
 }
