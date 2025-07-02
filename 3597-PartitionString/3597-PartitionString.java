8a1b176f78d3d046e633e9e7c6982aeeb6ba84cb
aakashdeepsinghgithub <117113183+aakashdeepsinghgithub@users.noreply.github.com>
2025-07-02 09:57:01 +0530

    File updated at 7/2/2025, 9:57:01 AM

--- 3597-PartitionString/3597-PartitionString.java
+++ 3597-PartitionString/3597-PartitionString.java
@@ -1,20 +1,23 @@
-// Last updated: 7/2/2025, 9:52:21 AM
+// Last updated: 7/2/2025, 9:57:01 AM
 import java.util.*;
 
 class Solution {
     public List<String> partitionString(String s) {
          Set<String> set = new HashSet<>();
          List<String> result = new ArrayList<>();
-         StringBuilder current = new StringBuilder();
+        //  StringBuilder current = new StringBuilder();
+        String ss="";
 
          for(char ch : s.toCharArray()){ 
-            current.append(ch);
-            String segment = current.toString();
-            if(!set.contains(segment)){ 
-                set.add(segment);
-                result.add(segment);
+            // current.append(ch);
+            ss+=ch;
+            // String segment = current.toString();
+            if(!set.contains(ss)){ 
+                set.add(ss);
+                result.add(ss);
 
-                current = new StringBuilder();
+                // current = new StringBuilder();
+                ss="";
             }
 
          }
