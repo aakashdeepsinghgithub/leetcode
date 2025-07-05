59d6ca48658b801302a78b7139781b9f25f74715
aakashdeepsinghgithub <117113183+aakashdeepsinghgithub@users.noreply.github.com>
2025-07-05 23:12:41 +0530

    Create 2320-FindAllKDistantIndicesInAnArray

---
+++ 2320-FindAllKDistantIndicesInAnArray/2320-FindAllKDistantIndicesInAnArray.java
@@ -0,0 +1,28 @@
+// Last updated: 7/5/2025, 11:12:41 PM
+class Solution {
+    public List<Integer> findKDistantIndices(int[] nums, int key, int k) {
+
+        Set<Integer> sett = new HashSet<>();
+
+
+        for(int i=0 ; i<nums.length; i++){
+
+            int start= Math.max(i-k,0);
+            int end =Math.min(nums.length-1,i+k);
+
+            for(int j = start ; j<=end ;j++) {
+
+            
+            
+            if(nums[j]==key){ 
+                sett.add(i);
+            }
+        }}
+         
+
+
+        List<Integer> result = new ArrayList<>(sett);
+        Collections.sort(result);
+        return result;
+    }
+}
