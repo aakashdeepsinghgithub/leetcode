c558da506c228fd6fcb2322d45bc2dec1e07f19d
aakashdeepsinghgithub <117113183+aakashdeepsinghgithub@users.noreply.github.com>
2025-07-30 23:38:41 +0530

    Create 10-RegularExpressionMatching

---
+++ 10-RegularExpressionMatching/10-RegularExpressionMatching.java
@@ -0,0 +1,36 @@
+// Last updated: 7/30/2025, 11:38:41 PM
+public class Solution {
+    public boolean isMatch(String s, String p) {
+        int m = s.length(), n = p.length();
+        boolean[][] dp = new boolean[m + 1][n + 1];
+
+        dp[0][0] = true; // both empty
+
+        // Fill for patterns like a*, a*b*, etc.
+        for (int j = 1; j <= n; j++) {
+            if (p.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 2];
+            }
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                char sc = s.charAt(i - 1);
+                char pc = p.charAt(j - 1);
+
+                if (pc == sc || pc == '.') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (pc == '*') {
+                    // zero occurrence
+                    dp[i][j] = dp[i][j - 2];
+                    // one or more if preceding matches
+                    if (p.charAt(j - 2) == sc || p.charAt(j - 2) == '.') {
+                        dp[i][j] = dp[i][j] || dp[i - 1][j];
+                    }
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+}
