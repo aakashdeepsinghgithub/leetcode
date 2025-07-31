55d4a5bedb845d01b397f267cda60dfcab960fdd
aakashdeepsinghgithub <117113183+aakashdeepsinghgithub@users.noreply.github.com>
2025-07-31 19:56:04 +0530

    File updated at 7/31/2025, 7:56:04 PM

--- 693-BinaryNumberwithAlternatingBits/693-BinaryNumberwithAlternatingBits.java
+++ 693-BinaryNumberwithAlternatingBits/693-BinaryNumberwithAlternatingBits.java
@@ -1,13 +1,11 @@
-// Last updated: 7/31/2025, 6:55:08 PM
+// Last updated: 7/31/2025, 7:56:04 PM
 class Solution {
     public boolean hasAlternatingBits(int n) {
-        int prev = -1;
-        while(n>0){
-            if(prev == (n & 1)) return false;
-            prev = (n & 1);
-            n >>= 1;
-        }
+        
+       n= n^(n >> 1);
 
-        return true;
+        int result = n & (n+1) ;
+        return result==0 ?  true : false;
+        
     }
 }
