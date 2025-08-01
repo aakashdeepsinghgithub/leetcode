08ea45d7860add58ba427f50db2e3963ec18b2c5
aakashdeepsinghgithub <117113183+aakashdeepsinghgithub@users.noreply.github.com>
2025-08-01 22:31:07 +0530

    Create 78-Subsets

---
+++ 78-Subsets/78-Subsets.java
@@ -0,0 +1,24 @@
+// Last updated: 8/1/2025, 10:31:08 PM
+class Solution {
+    List<List<Integer>> res = new ArrayList<List<Integer>>();
+
+    
+    void subsetUtility(int[] nums , List<Integer> temp, int index ){ 
+
+        if(index >= nums.length){ 
+            res.add(new ArrayList<Integer>(temp));
+            return;
+        }
+
+        temp.add(nums[index]);
+        subsetUtility(nums,temp,index+1);
+        temp.remove(temp.size()-1);
+        subsetUtility(nums,temp,index+1);
+    }
+
+    public List<List<Integer>> subsets(int[] nums) {
+        List<Integer> temp = new ArrayList<>();
+        subsetUtility(nums,temp,0);
+        return res;
+    }
+}
